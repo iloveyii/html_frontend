ace4e350ae64359ae21871f045e815749ea12201
ali <ali@ubuntu.ubuntu-domain>
2014-04-12 18:48:26 +0200

    after simple requireJS  - working

---
+++ js/common.js
@@ -0,0 +1,19 @@
+
+
+require.config({
+    baseUrl: "./js",
+    paths: {
+        // the libraries we use
+        jquery: [
+            '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min',
+            // if the cdn fails load the local copy of jquery
+            'lib/jquery'
+        ],
+        bootstrap: 'lib/bootstrap'
+    }
+    
+    
+    
+});
+
+
